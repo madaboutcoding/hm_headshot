a1038ccf063976572c773919565f102695efd414
madaboutconfig <madaboutconfig@gmail.com>
2022-11-04 21:04:36 +0530

    commit jquerydemo2

---
+++ website16/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+   //Tag Selector 
+   $("button").on("click",()=>{
+        $("h1").css("backgroundColor","navy");
+        $("h1").css("color","white");
+   });
+
+
+});
