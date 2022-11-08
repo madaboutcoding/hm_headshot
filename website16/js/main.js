3efa07e3dd38a547bd1f43e494ce74c4610143ee
madaboutconfig <madaboutconfig@gmail.com>
2022-11-08 22:11:25 +0530

    part1

--- website16/js/main.js
+++ website16/js/main.js
@@ -1,9 +1,27 @@
 $(document).ready(function(){
    //Tag Selector 
-   $("button").on("click",()=>{
-        $("h1").css("backgroundColor","navy");
-        $("h1").css("color","white");
-   });
+//    $("button").on("click",()=>{
+//         $("h1").css("backgroundColor","navy");
+//         $("h1").css("color","white");
+//         // $(":button").hide();
+//         $("button:even").hide();
+//    });
 
+// $("button").click(function(){
+//     $("h1").css("backgroundColor","yellow").css("color","navy");
+// });
 
+$("button").on(
+    {
+    mouseenter: function(){
+      $("h1").css("background-color", "lightgray");
+    },
+    mouseleave: function(){
+      $("h1").css("background-color", "lightblue");
+    },
+    click: function(){
+      $("h1").css("background-color", "yellow");
+    }
+  }
+  );
 });
