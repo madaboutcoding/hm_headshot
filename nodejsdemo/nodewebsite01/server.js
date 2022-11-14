f039f31b6efed60a9f66fa0c71851bd9b16ec58a
madaboutconfig <madaboutconfig@gmail.com>
2022-11-14 22:40:26 +0530

    updatenodewebsite

--- nodejsdemo/nodewebsite01/server.js
+++ nodejsdemo/nodewebsite01/server.js
@@ -5,10 +5,22 @@ const fs=require('fs');
 const port=3000;
 
 http.createServer((req,resp)=>{
-    resp.setHeader("Content-Type","text/text");
-    resp.write("<strong>Welcome to the world of node</strong>");
-    resp.write("<strong>Welcome to the world of node</strong>");
-    resp.end();
+    // resp.setHeader("Content-Type","text/text");
+    // resp.write("<strong>Welcome to the world of node</strong>");
+    // resp.write("<strong>Welcome to the world of node</strong>");
+    // resp.end();
+    fs.readFile(__dirname+"/index.html",(err,data)=>{
+        if(err){
+            resp.setHeader("Content-Type","text/html");
+            resp.write("Error: "+err);
+            resp.end();
+        }else{
+            resp.setHeader("Content-Type","text/html");
+            resp.write(data);
+            resp.end();
+        }
+    })
+
 }).listen(port,()=>{
     console.log(`Hi Im alive at http://localhost:${port}`);
 })
